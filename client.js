29ff1df1973913921dce5b36537d0e193296d99b
“mehmetkah” <mehmetkahca@gmail.com>
2022-08-24 21:30:05 -0600

    Stind Setup

--- client.js
+++ client.js
@@ -1,25 +1,25 @@
-
-
+const { write } = require("fs");
 const net = require("net");
 
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: '10.0.0.169' ,
-    port: 50541
-  
+    host: "10.0.0.169",
+    port: 50541,
   });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
   conn.on("connect", () => {
-   console.log("Succesfully connected!")
-  conn.write('Name: Mfk')
+    console.log("Succesfully connected!");
+    conn.write("Name: Mfk");
+    conn.write("Move: up");
+    /*conn.write("Move: left");
+    conn.write("Move: down");
+    conn.write("Move: right");*/
   });
-  
+
   return conn;
-  
 };
 
-
-module.exports= connect
+module.exports = connect;
--- play.js
+++ play.js
@@ -1,4 +1,19 @@
-const connect = require("./client")
-console.log("Connecting")
-const conn = connect()
+const connect = require("./client");
+console.log("Connecting");
+const conn = connect();
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
 
+
+const handleUserInput = function () {
+   if (key==='\u0003') {
+       process.exit()
+   }return
+  };
+  setupInput()
